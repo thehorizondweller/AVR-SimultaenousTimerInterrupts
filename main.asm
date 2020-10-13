436ac9a776944df2a87420ee71605435c54cf7c5
Kshitij Anand <kshitijanand.iitkgp@gmail.com>
2020-10-13 10:40:57 +0530

    Uploaded Final Updated Solutions

--- main.asm
+++ main.asm
@@ -1,109 +1,116 @@
 ;
-; Assignment 1.asm
+; FECS-Module2-A1.asm
 ;
-; Created: 12-10-2020 08:37:24
+; Created: 12-10-2020 16:39:47
 ; Author : Kshitij Anand
 ;
 
-.INCLUDE "M32DEF.INC"
-.ORG $0
-	JMP MAIN
-.ORG $0E
-	JMP T1_OC_ISR
-.ORG $12
-	JMP T1_OV_ISR
-.ORG $16
-	JMP T0_OV_ISR
+.include "M32DEF.INC"
 
-;MAIN PROGRAM FOR INITIALIZATION AND KEEPING THE CPU BUSY WITH GENERAL TASK
-.ORG $100
-MAIN:
-	LDI R20, HIGH(RAMEND)
-	OUT SPH, R20
-	LDI R20, LOW(RAMEND)
-	OUT SPL, R20 ;INITIALIZATION OF STACK POINTER IS COMPLETE
-	SBI DDRB, 3  ;PB3 AS OUTPUT
-	LDI R20, (1<<TOIE0)|(1<<TOIE1)
-	OUT TIMSK, R20 ;ENABLE TIMER0 AND TIMER1 OVERFLOW INTERRUPT FLAGS IN TIMSK REG
-	SEI            ;SET I (ENABLE INTERRUPTS GLOBALLY)
-	;Timer0----------------------
-	LDI R20, (-125) ;value for 0.016 secs
-	OUT TCNT0, R20 ;initializing TCNT0 to overflow after 125 clocks
-	LDI R20, $05
-	OUT TCCR0, R20 ;Normal mode, int clk, prescaler 1:1024
-	;Timer1----------------------------
-	LDI R20, HIGH(-6250)
-	OUT TCNT1H, R20 ;setting high byte
-	LDI R20, LOW(-6250)
-	OUT TCNT1L, R20 ;setting low byte
-	LDI R20, $00
-	OUT TCCR1A, R20
-	LDI R20, $05
-	OUT TCCR1B, R20   ;normal mode with prescaler as 1:1024
-	;----------------------
-	LDI R20, $FF
-	OUT DDRD, R20 ;PORTD as output
-	;----------------------
-	;Infinite loop which keeps outputing 0x11 on PORTD
-HERE:
-	LDI R16, $11
-	OUT PORTD, R16 ;keep outputing 0x11 in non-interrupted situations
-	RJMP HERE
-;-----------------------
-;ISR for Timer0 Overflow - LOWER PRIORITY
-.ORG $200
-T0_OV_ISR:
-	LDI R20, (-125)
-	OUT TCNT0, R20 ;load timer0 for next round
-	LDI R20, 1<<TOV0
-	IN R22, TIFR
-	EOR R22, R20
-	OUT TIFR, R22 ;Reset the TOV0 flag in TIFR
-	RETI 
-;------------------------------
-;ISR for Timer1 Overflow - HIGHER PRIORITY
-.ORG $300
-T1_OV_ISR:
-	LDI R20, HIGH(-6250)
-	OUT TCNT1H, R20 ;resetting high byte for next round
-	LDI R20, LOW(-6250)
-	OUT TCNT1L, R20 ;resetting low byte for next round
-	IN R20, TIFR
-	SBRS R20, TOV0 ;check if Timer0 overflowed
-	RETI
-	LDI R20, $08 
-	IN R22, PORTB
-	EOR R22, R20 ;toggle the fourth bit
-	OUT PORTB, R22
-	SEI
-	;Procedure for outputing 0x22 on PORTD after 10 ms
-	;For this part we will have to use CTC mode
-	;When Compare ISR is executed we shift back to Normal mode
-	LDI R20, HIGH(-5000)
-	OUT OCR1AH, R20
-	LDI R20, LOW(-5000)
-	OUT OCR1AL, R20
-	LDI R20, $00
-	OUT TCCR1A, R20
-	LDI R20, $0D
-	OUT TCCR1B, R20   ;CTC mode with prescaler as 1:1024
-	;Now this will call the Compare ISR as soon as 1250 clocks are clocked
-	SEI ; this required because compare match has lower priority than overflow interrupt
-	RETI
+.org 0x0
+	jmp main
+.org 0x12
+	jmp t1_ov_isr
+.org 0x16
+	jmp t0_ov_isr
 
-;-----------------------------
-;ISR for Timer1 CTC mode
-.ORG $400
-T1_OC_ISR:
-	LDI R20, $22
-	OUT PORTD, R20
-	LDI R20, HIGH(-5000);Clock the remaining 5000 clocks
-	OUT TCNT1H, R20 ;setting high byte
-	LDI R20, LOW(-5000)
-	OUT TCNT1L, R20 ;setting low byte
-	LDI R20, $00
-	OUT TCCR1A, R20
-	LDI R20, $04
-	OUT TCCR1B, R20   ;shift back to normal mode with prescaler as 1:1024
-	RETI
+;main program 
+main:
+	ldi r20, high(ramend)
+	out sph, r20
+	ldi r20, low(ramend)
+	out spl, r20 ;stack pointer initialised
+
+	sbi ddrb, 3 ;set PB3 as output
+
+	ldi r20, -125 ;time for 0.016 seconds
+	out tcnt0, r20
+	ldi r20, high(-6250) ;time for 0.8 seconds
+	out tcnt1h, r20
+	ldi r20, low(-6250)
+	out tcnt1l, r20
+	ldi r20, 0x00
+	out tccr1a, r20
+	ldi r20, 0x05
+	out tccr1b, r20 ;init clk, normal mode, prescaler 1024
+	out tccr0, r20 ;init clk, normal mode, prescaler 1024
+
+	ldi r20, (1<<toie0)|(1<<toie1)
+	out timsk, r20 ;enabling overflow interrupts
+	sei ;interrupts enabled gloablly
+
+	ldi r20, 0xff
+	out portd, r20 ;PORTD as output
+
+	;defining two custom flags to check coincidence
+	ldi r24, 0x00 ;will be set when timer0 overflows
+	ldi r25, 0x00 ;will be set when timer1 overflows
+
+	;infinite loop to output 0x11 on PORTD
+here:
+	ldi r20, 0x11
+	out portd, r20
+	rjmp here
+
+.org 0x100
+t0_ov_isr:
+	ldi r20, -125 ;load timer0 for next round
+	out tcnt0, r20
+	ldi r24, 0x01
+	call coincidence
+	reti
+
+.org 0x200
+t1_ov_isr:
+	ldi r20, high(-6250) ;load timer1 for next round
+	out tcnt1h, r20
+	ldi r20, low(-6250)
+	out tcnt1l, r20
+	ldi r25, 0x01
+	sei
+	call coincidence
+	reti
+
+.org 0x300
+coincidence:
+	sei
+	mov r16, r24
+	mov r17, r25
+	eor r16, r17 ;check if both last bits are 1
+	sbrs r16, 0
+	call procedure
+	ldi r24, 0x00 ;resetting the flags
+	ldi r25, 0x00
+	ret
+
+.org 0x400
+procedure: ;called only if conincidence occurs
+	ldi r20, 0x08
+	in r22, portb 
+	eor r22, r20 ;toggles bit 3 of port b
+	out portb, r22
+	call delay
+	ldi r20, 0x22
+	out portd, r20 ;output 0x22 on portd 10 ms after coincidence
+	ret
+
+.org 0x500
+delay:
+	;we will use timer2 because we cannot disturb timer0 and timer1
+	ldi r20, -78 ; time for 10 ms with 1024 as prescaler
+	out tcnt2, r20; load timer2
+	ldi r20, 0x07
+	out tccr2, r20 ;timer2, normal mode, init clk, prescaler 1024
+again:
+	in r20, tifr ;read tifr register
+	sbrs r20, tov2 ;if tov2 is set, skip next instruction
+	rjmp again
+	ldi r20, 0x00 
+	out tccr2, r20 ;stop timer2
+	ldi r20, 1<<tov2
+	out tifr, r20 ;clear tov2 flag
+	ret
+
+
 	
+
